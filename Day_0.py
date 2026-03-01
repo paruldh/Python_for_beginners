c204df5befc0efec62036c1435c18ea7c16c91eb
paruldh <meparuldhiman@gmail.com>
2026-03-01 23:08:16 +0530

    Day0

---
+++ Day_0.py
@@ -0,0 +1,12 @@
+#Markdown Basics
+# Heading 1 
+## Heading 2
+### Heading 3
+# for bold use **BOLD**
+# for italics use *Italics*
+# for ordered lists of tasks
+tasks = ["Wake up","eat","study","sleep"]
+print(tasks)
+# for unordered lists of tasks
+Fruits = ["apple","banana","cherry"]
+print(Fruits) 
