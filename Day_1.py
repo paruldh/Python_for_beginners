7b3c0462b38d9d87c70f715a5f37504e880ebbc4
paruldh <meparuldhiman@gmail.com>
2026-03-02 23:36:03 +0530

    day_1.py

---
+++ Day_1.py
@@ -0,0 +1,28 @@
+#Python Indentation: refers to the spaces at the beginning of a code line.
+#Python uses indentation to indicate a block of code.
+if 5>2:
+    print("FIVE IS GREATER THAN TWO")
+#In python variables are created when you assign a value to it.
+age=17
+print(age)
+#variables are containers for storing data.
+#variable is created the moment you first assign a value to it.
+x=16 #x is an int data type 
+y=15 #y is an int data type
+sum= x+y
+print(sum)
+#data types, text type = str, numeric type = int,float, complex, sequence type = list,tuple.
+#Text type = str
+name="parul"
+print(name)
+print(type(name))
+#Numeric type = int,float, complex    
+x= 1.10
+y= 23
+z= -23.5
+print(type(x))
+print(type(y))
+print(type(z))
+print(x)
+print(y)
+print(z)
