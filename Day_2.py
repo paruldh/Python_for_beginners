2b0d2d2563e9bc9d53a76290d7c3fa972f3a7700
paruldh <meparuldhiman@gmail.com>
2026-03-03 23:58:36 +0530

    Day_2

---
+++ Day_2.py
@@ -0,0 +1,11 @@
+#Python Terms to know:
+#object, variable, function,class, method,attribute
+90000 # this is an object
+"Data Enginner" #this is an object
+type("Data Enginner")
+print(type)
+job_salary = 95000
+job_title = "social media manager"
+print(job_salary)
+print(job_title)
+
